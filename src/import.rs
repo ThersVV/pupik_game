e2a0b601e111b9f9aa22cf18acd797e454282712
Vojta2 <nomovl@seznam.cz>
2023-08-31 12:43:52 +0200

    change file name

--- src/import.rs
+++ src/import.rs
@@ -6,11 +6,11 @@ use std::fs::*;
 /// * `asvec_server` - [AsvecServer], used to load the music in.
 /// * `audio` - [Audio].
 pub fn import_structures() -> Option<Vec<Structure>> {
-    if let Err(_) = create_dir_all("./import") {
+    if let Err(_) = create_dir_all("./structures") {
         return None;
     };
 
-    let Ok(paths) = read_dir("./import") else {return None;};
+    let Ok(paths) = read_dir("./structures") else {return None;};
     let mut result = Vec::from([]);
     for path in paths {
         let mut vec = Vec::from([]);
@@ -18,7 +18,7 @@ pub fn import_structures() -> Option<Vec<Structure>> {
         let mut first = true;
         let mut min_y = i32::MAX;
         for line in read_to_string(path.unwrap().path())
-            .expect("Error reading file \"export\"")
+            .expect("Error reading file \"structures\"")
             .lines()
         {
             if first {
@@ -28,7 +28,7 @@ pub fn import_structures() -> Option<Vec<Structure>> {
                 continue;
             }
             let line_split = line.split(' ').collect::<Vec<&str>>();
-            let error_message = "Incorrect formatting of the \"export\" file";
+            let error_message = "Incorrect formatting of the \"structures\" file";
             let x = Some(line_split[0].parse::<i32>().expect(error_message));
             let y = Some(line_split[1].parse::<i32>().expect(error_message));
             if y.unwrap() < min_y {
